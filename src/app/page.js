d3b732296f155ec80d188872f7bfa893c7e8562a
GaganVashishth <gagan.akrolix@gmail.com>
2023-12-19 09:41:01 +0530

    working on set keys of thailand forms

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import BannerMain from "@/components/main/BannerMain";
-
+import Footer from "@/components/main/Footer";
+import Header from "@/components/main/Header";
 
 export default function Home() {
   return (
     <div className="">
+      <Header />
       <BannerMain />
+      <Footer />
     </div>
   );
 }
