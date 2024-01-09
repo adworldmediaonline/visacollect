dde7dc1d20b1409461a5b1e52ec381e7227c7a68
Anjali1sh <anjali@ackrolix.com>
2024-01-09 15:10:55 +0530

    singapore pages

--- src/app/singapore/page.js
+++ src/app/singapore/page.js
@@ -14,7 +14,7 @@ function Page() {
         validity=" Valid for 1 year"
         entries="Single Entries"
         price="$126.67"
-        link="/singapore/application"
+        link="/singapore/embassy-application"
       />
       <ProcessingStep processingData={processingData1} />
       <LearnMore learnMoreSectionData={learnMoreSectionData} />
