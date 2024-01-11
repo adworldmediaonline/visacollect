a7d25ba0729f49e2a47e4784ba214e0333c2fb4c
fastestsunil <sunil141292@gmail.com>
2024-01-11 18:00:46 +0530

    Refactor form validation and add Indonesia schema

--- src/app/oman/step-two/page.js
+++ src/app/oman/step-two/page.js
@@ -108,8 +108,6 @@ function Page() {
             >
               {({ values, isValid, setFieldValue }) => (
                 <Form>
-                  {console.log(values)}
-
                   <SubHeading subHead="Personal Details" />
 
                   <div className="main-form-section">
