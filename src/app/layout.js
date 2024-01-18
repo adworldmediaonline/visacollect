34e5d4ba60297bd95633c81e3e127a2a685fdfd9
Anjali1sh <anjali@ackrolix.com>
2024-01-18 10:30:02 +0530

    site verification link

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="google-site-verification" content="qsi8k0I-otMOh5FASZcM5X9KLygQS5gqWtSD9H4Qnxc" />
+      </Head>
       <body className={`${poppins.className} antialiased`}>
         <FormProvider>
           <ReactQueryProvider>
