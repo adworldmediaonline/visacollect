770be9b7f9cfa2b1c0ab8d8157657fcb12d785ae
fastestsunil <sunil141292@gmail.com>
2024-01-31 16:03:58 +0530

    australia home page responsiveness

--- src/app/australia/page.js
+++ src/app/australia/page.js
@@ -32,21 +32,24 @@ const Page = () => {
         link="/australia/application"
       /> */}
       <div className="w-full h-[0.5px] bg-gray-200"></div>
-      <div className="flex">
-        <main className="flex-1 py-2 [&_strong]:text-[#595b66] [&_p]:font-normal [&_p]:text-[#343a40] [&_li]:text-[#343a40]">
+      <div className="flex flex-col md:flex-row">
+        <main className="flex-1 py-2 [&_strong]:text-tertiary [&_p]:font-normal [&_p]:text-[#343a40] [&_li]:text-[#343a40]">
           <LearnMore learnMoreSectionData={learnMoreSectionDataAustralia} />
         </main>
-        <aside className="basis-[300px]  py-12 space-y-4">
-          <div>Other links:</div>
-          <ul className="flex flex-col gap-3">
-            {learnMoreSectionDataAustralia?.otherLinks?.map((link, index) => (
-              <li key={index}>
-                <Link className="underline text-primary" href={link.path}>
-                  {link.linkName}
-                </Link>
-              </li>
-            ))}
-          </ul>
+        <aside className="basis-[300px] py-5 md:py-12 space-y-4">
+          <div className="container">
+            {' '}
+            <div>Other links:</div>
+            <ul className="flex flex-col gap-3">
+              {learnMoreSectionDataAustralia?.otherLinks?.map((link, index) => (
+                <li key={index}>
+                  <Link className="underline text-primary" href={link.path}>
+                    {link.linkName}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </aside>
       </div>
       <div className="mt-16">
