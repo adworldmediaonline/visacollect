48bca6fc1efc9512fa96818fdb3b058f0e44efb0
fastestsunil <sunil141292@gmail.com>
2024-02-02 11:53:11 +0530

    oman home page

---
+++ src/constant/countryHomePageData/oman.js
@@ -0,0 +1,221 @@
+import { FaArrowRightLong } from 'react-icons/fa6';
+import { visaInfo } from '../images';
+import CustomButton from '@/components/common/CustomButton';
+export const learnMoreSectionDataOman = {
+  pageTitle:
+    '<strong>Oman Visa:</strong> Entry requirements and travel information for Oman',
+  pageName: 'Oman',
+  applyNowLink: '/oman/step-one',
+  otherLinks: [
+    {
+      linkName: 'Easy as a walkabout: The Oman visa options',
+      path: '/',
+    },
+    {
+      linkName: 'Oman Visa application process: No worries, mate!',
+      path: '/',
+    },
+    {
+      linkName: 'Oman entry requirements',
+      path: '/',
+    },
+  ],
+  visaRequired: {
+    visaRequiredText:
+      'You need a Visa to travel to Oman if you have a passport from United States.',
+    visaRequiredCards: [
+      {
+        validity: 'Valid for 1 Year',
+        title: 'ETA',
+        entryType: 'Multiple Entry',
+        governmentFee: {
+          label: 'Government Fee',
+          amount: 'USD $13.18',
+          perApplicant: '/Applicant',
+        },
+      },
+    ],
+  },
+  // visaRequirements: {
+  //   title: 'Australia Visa Requirements to Apply for your e Visa',
+  //   list: [
+  //     "Applicant's Photo",
+  //     'National ID',
+  //     'Passport page ',
+  //     'All passport pages ',
+  //     'Scans of the passport pages that have immigration stamps',
+  //     'Legal Guardian Passport Scan (if applicable)',
+  //     ' Bank statements from the last 3 months',
+  //     '  Bank statement (if applicable)',
+  //     'Airline confirmation',
+  //     'Proof of accommodation ',
+  //     ' Employment Proof (if applicable)',
+  //     'Book of Family (if applicable)',
+  //     'Invitation Letter (if applicable)',
+  //     'Cover Letter',
+  //     ' Supporting Document (if applicable)',
+  //     'Travel itinerary (if applicable)',
+  //     'Birth certificate (if applicable)',
+  //     ' Proof of Marriage or Spousal Relationship (if applicable)',
+  //     ' Certificate of Incorporation (if applicable)',
+  //   ],
+  // },
+  sections: [
+    {
+      title: '<h2>Discover Hassle-Free Travel to Oman with e-Visa!</h2>',
+      paragraphs: [
+        {
+          text: '<strong>Are you a GCC resident eager to explore Oman?</strong>',
+        },
+        {
+          text: '<strong>Good news!</strong> Thanks to our streamlined online visa system, your journey just got smoother.',
+        },
+        {
+          text: 'Introducing the Oman Visa for GCC Residents – a convenient single-entry travel document valid for 30 days with a maximum stay of 28 days per entry. Ideal for tourist activities, this visa comes with a lower fee, reflecting the strong diplomatic ties between Oman and GCC countries.',
+        },
+      ],
+    },
+    {
+      title: '<h2>Why choose e-Visa?</h2>',
+      paragraphs: [
+        {
+          text: '<strong>#1. Government Compliance:</strong> Our team ensures that your application aligns 100% with government requirements.',
+        },
+        {
+          text: '<strong>#2. Simplified Process:</strong> Forget confusing embassy forms. Our user-friendly platform offers a clear, concise application process, akin to filling out a short survey.',
+        },
+        {
+          text: '<strong>#3. Flexibility:</strong> Fill out the form at your pace, make changes whenever needed, and save your progress. No need to start from scratch.',
+        },
+        {
+          text: '<strong>#4. Secure & Trusted:</strong> Your safety is our priority. Benefit from our robust security system, and check out our customer reviews for peace of mind.',
+        },
+      ],
+    },
+    {
+      title: '<h2>Who can apply?</h2>',
+      paragraphs: [
+        {
+          text: 'Residents of Bahrain, Kuwait, Qatar, Saudi Arabia, or UAE can apply, encompassing over 200 nationalities. Just hold a GCC resident permit.',
+        },
+        {
+          text: '<strong>Benefits:</strong>',
+        },
+        {
+          text: '<strong>Cost-Effective:</strong> Only $13 USD for the government fee.',
+        },
+        {
+          text: `<strong>Error-Free:</strong> Our team minimizes errors, ensuring a smooth entry to Oman.`,
+        },
+      ],
+    },
+    {
+      title: '<h3>How to Apply:</h3>',
+      paragraphs: [
+        {
+          text: `<strong>Step-1:</strong> Complete our simplified form with personal details, required documents, and travel information.`,
+        },
+        {
+          text: `<strong>Step - 2:</strong> Pay conveniently with credit/debit card or PayPal.`,
+        },
+        {
+          text: `<strong>Step - 3:</strong> Attach remaining documents and click "Submit."`,
+        },
+        {
+          text: `<strong>Step - 4:</strong> Await processing, receive your Oman Visa for GCC Residents, and prepare for an exciting journey!`,
+        },
+      ],
+    },
+    {
+      title:
+        '<h3>Single or Multiple Entry? Choose Your Adventure As You May!</h3>',
+      paragraphs: [
+        {
+          text: `One trip or multiple Nile cruises? Single-entry lasts 30 days, while the multiple lets you return for a year (perfect for business or frequent fliers).`,
+        },
+      ],
+    },
+    {
+      lists: [
+        {
+          title: '<strong>Required Documents:</strong>',
+          listItems: [
+            `GCC Residence Permit`,
+            `Passport scan (valid for at least 6 months)`,
+            `Free last page of passport (if applicable)`,
+            `Birth Certificate of Minor (if applicable)`,
+            `Airline confirmation (if applicable)`,
+            `Proof of accommodation (if applicable)`,
+            `Applicant's Photo`,
+            `Letter of invitation from host or sponsor (if applicable)`,
+          ],
+        },
+      ],
+
+      paragraphs: [
+        {
+          text: `<strong>Important Points:</strong>`,
+        },
+        { text: `- Stay can be extended once.` },
+        { text: `- Entry must be from a GCC country.` },
+        { text: `- No change to a work visa while in Oman.` },
+        {
+          text: `- No need for a sponsor, but your profession must be on the approved list.`,
+        },
+        {
+          text: `<strong>Children & Family Travel:</strong>`,
+        },
+        {
+          text: `- Children can apply.`,
+        },
+        {
+          text: `- Parents or legal guardians must present a valid Oman visa or apply first before submitting a child's application.`,
+        },
+        {
+          text: `- If applying with a spouse, the applicant must get the visa issued first.`,
+        },
+      ],
+    },
+    {
+      title: '<h3>Ready to embark on your adventure?</h3>',
+      paragraphs: [
+        {
+          text: `<strong>Apply now with e-Visa – Your Trusted Partner for Effortless Travel!</strong>`,
+        },
+        {
+          text: `We are dedicated to providing a hassle-free and transparent experience. If you have any questions or need further assistance, feel free to contact our 24/7 customer support. Your satisfaction is our priority!`,
+        },
+        {
+          href: 'mailto:help@e-visa.com',
+          linkText: `help@e-visa.com`,
+          text: `For more information or assistance, contact our 24/7 customer support at <span class="email-link">help@visacollect.com</span> or explore our FAQs below.`,
+        },
+        {
+          text: `<strong>Start Your Journey with e-Visa Now!</strong>`,
+        },
+      ],
+    },
+  ],
+  faqData: [
+    {
+      id: 1,
+      title: 'Having trouble completing your application due to an error?',
+      des: `We understand the frustration, and our dedicated customer service team is here to assist you promptly. Reach out as soon as possible, and our visa experts will guide you through resolving the issue to ensure a smooth process.`,
+    },
+    {
+      id: 2,
+      title: `What If Your Oman GCC Resident eVisa Application Faces Cancellation or Rejection?`,
+      des: `At e-Visa, we meticulously review your documents to minimise rejections. However, should the Omani government reject or cancel your application, rest assured that you are entitled to a full refund of the e-Visa processing fee associated with the number of rejected applications. Note that government fees are non-refundable.`,
+    },
+    {
+      id: 3,
+      title: `Considering a Reapplication for an Oman GCC Resident Visa?`,
+      des: `Depending on the reason for cancellation or rejection, you might contemplate reapplying for an Oman GCC Resident Visa. Our support team can provide guidance on the best course of action in such cases. We are committed to assisting you throughout the process.`,
+    },
+    {
+      id: 4,
+      title: `Made a Mistake in Your Answers? Modification is Possible!`,
+      des: `If you realize an error in your answers, don't worry. Contact our customer support team promptly, and we'll help you rectify the situation. Remember, our expert team thoroughly reviews each application, ensuring that any necessary adjustments are made before submission. You can trust us to guide you through the correction process seamlessly.`,
+    },
+  ],
+};
