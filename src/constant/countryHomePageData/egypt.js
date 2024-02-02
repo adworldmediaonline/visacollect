6e96316ca21a8168edea6368310bde72403535f0
fastestsunil <sunil141292@gmail.com>
2024-02-02 11:26:54 +0530

    egypt home page

---
+++ src/constant/countryHomePageData/egypt.js
@@ -0,0 +1,193 @@
+import { FaArrowRightLong } from 'react-icons/fa6';
+import { visaInfo } from '../images';
+import CustomButton from '@/components/common/CustomButton';
+export const learnMoreSectionDataEgypt = {
+  pageTitle:
+    '<strong>Egypt Visa:</strong> Entry requirements and travel information for Egypt',
+  pageName: 'Egypt',
+  applyNowLink: '/egypt/application',
+  otherLinks: [
+    {
+      linkName: 'Easy as a walkabout: The Egypt visa options',
+      path: '/',
+    },
+    {
+      linkName: 'Egypt Visa application process: No worries, mate!',
+      path: '/',
+    },
+    {
+      linkName: 'Egypt entry requirements',
+      path: '/',
+    },
+  ],
+  visaRequired: {
+    visaRequiredText:
+      'You need a Visa to travel to Egypt if you have a passport from United States.',
+    visaRequiredCards: [
+      {
+        validity: 'Valid for 1 Year',
+        title: 'ETA',
+        entryType: 'Multiple Entry',
+        governmentFee: {
+          label: 'Government Fee',
+          amount: 'USD $13.18',
+          perApplicant: '/Applicant',
+        },
+      },
+    ],
+  },
+  // visaRequirements: {
+  //   title: 'Australia Visa Requirements to Apply for your e Visa',
+  //   list: [
+  //     "Applicant's Photo",
+  //     'National ID',
+  //     'Passport page ',
+  //     'All passport pages ',
+  //     'Scans of the passport pages that have immigration stamps',
+  //     'Legal Guardian Passport Scan (if applicable)',
+  //     ' Bank statements from the last 3 months',
+  //     '  Bank statement (if applicable)',
+  //     'Airline confirmation',
+  //     'Proof of accommodation ',
+  //     ' Employment Proof (if applicable)',
+  //     'Book of Family (if applicable)',
+  //     'Invitation Letter (if applicable)',
+  //     'Cover Letter',
+  //     ' Supporting Document (if applicable)',
+  //     'Travel itinerary (if applicable)',
+  //     'Birth certificate (if applicable)',
+  //     ' Proof of Marriage or Spousal Relationship (if applicable)',
+  //     ' Certificate of Incorporation (if applicable)',
+  //   ],
+  // },
+  sections: [
+    {
+      title:
+        '<h2>Climb the Magnificent Pyramids with an Easy Online Visa from e-Visa</h2>',
+      paragraphs: [
+        {
+          text: '<strong>No Embassy Waltz Required!</strong>',
+        },
+        {
+          text: 'If you’re dreaming of camel treks through sand dunes and basking in Nile-side sunshine, hold that thought right there, because we’re making it happen.',
+        },
+        {
+          text: `Hold a pyramid-shaped souvenir – you need an Egypt e-Visa first! But ditch the embassy lines and stuffy forms, because this visa's smoother than a sun-warmed Sphinx.`,
+        },
+      ],
+    },
+    {
+      title: '<h2>What is this Magic eVisa?</h2>',
+      paragraphs: [
+        {
+          text: 'Think of it as your digital key to unlock ancient wonders and modern marvels. Forget consulate dramas, just apply online in three easy steps (seriously, less than fifteen minutes is all it takes!).',
+        },
+      ],
+      image: visaInfo,
+      imageAlt: 'visainfo1',
+    },
+    {
+      title: '<h2>What Can You Do with this Visa Power?</h2>',
+      paragraphs: [
+        {
+          text: '<strong>Unleash your inner Indiana Jones:</strong> Explore tombs, temples, and hieroglyph-filled walls.',
+        },
+        {
+          text: '<strong>Chill like a Pharaoh:</strong> Relax on pristine beaches, snorkel in dazzling coral reefs, and sip mint tea in vibrant bazaars.',
+        },
+        {
+          text: '<strong>Business with a Bling:</strong> Seal the deal with a Nile view or network at swanky Cairo conferences.',
+        },
+      ],
+    },
+    {
+      title: '<h2>But Wait, Are You Eligible?</h2>',
+      paragraphs: [
+        {
+          text: `Check our nifty Visa Checker Tool – if your country's on the list (over 70 and counting!), you're golden. Some lucky folks (US, EU, UK) in specific Red Sea resorts skip the visa for 14 days – sunburns only, please!`,
+        },
+        {
+          text: `<strong>Got Kids? No Problem!</strong>`,
+        },
+        {
+          text: `They need their own eVisa too (even toddlers), but you can fill out their forms like a pro parent.`,
+        },
+        {
+          text: `<strong>Note:</strong> If the passport holder has children under 16 years old included in the passport, only one visa is necessary. `,
+        },
+      ],
+    },
+    {
+      title:
+        '<h3>Single or Multiple Entry? Choose Your Adventure As You May!</h3>',
+      paragraphs: [
+        {
+          text: `One trip or multiple Nile cruises? Single-entry lasts 30 days, while the multiple lets you return for a year (perfect for business or frequent fliers).`,
+        },
+      ],
+    },
+    {
+      title: '<h3>Visa Fees: Transparent and Easy Peasy</h3>',
+      paragraphs: [
+        {
+          text: `We offer three processing speeds (standard, rush, super rush – think instant gratification for travel addicts!). Prices cover everything, including government fees, so no hidden surprises.`,
+        },
+      ],
+    },
+    {
+      title: '<h2>Need to Extend Your Visa?</h2>',
+      paragraphs: [
+        {
+          text: `Head to the Cairo immigration office (early bird gets the worm!), but online extensions aren't an option. Remember, overstaying is a no-no, so keep an eye on your visa's expiry date.`,
+        },
+        {
+          text: `<strong>Packing Checklist</strong>`,
+        },
+        {
+          text: `<strong>Valid Passport:</strong> Six months' life left is a must, and a blank page for those official stamps.`,
+        },
+        {
+          text: `<strong>Printed eVisa:</strong> Don't rely on Wi-Fi in the desert, take a hard copy.`,
+        },
+        {
+          text: `<strong>Travel Plans:</strong> Itinerary, supporting letters (business or family visits), and accommodation details.`,
+        },
+      ],
+    },
+    {
+      title: '<h2>Ready to Make Egypt Yours?</h2>',
+      paragraphs: [
+        {
+          text: `Our friendly customer service team is here 24/7 to answer any questions and guide you through the process. So, grab your passport, dust off your explorer hat, and let's get your Egypt eVisa rolling!`,
+        },
+      ],
+    },
+  ],
+  faqData: [
+    {
+      id: 1,
+      title: 'Application Glitches Got You Down?',
+      des: `Don't sweat it! Technical hiccups happen, but our customer-centric team of e-Visa experts is on standby 24/7 to help you get back on track. Just drop them a few texts, and they'll guide you through the fix like seasoned pros. You'll be back to your Egypt-visa journey in no time!`,
+    },
+    {
+      id: 2,
+      title: `Application Denied? Here's What to Know.`,
+      des: `Rejection can sting, even when it comes to visas. But here's the good news: if the government nixes your application, we'll gladly refund the iVisa processing fee for you (though government fees are a separate story).`,
+      extraContent: [
+        {
+          text: `<strong>Remember:</strong> Our support team is always here to answer questions and help explore other options`,
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: `Visa in Hand, But Entry Not Guaranteed?`,
+      des: `Think of your visa as your golden ticket to Egypt's adventures, but remember, the final decision rests with the border officials. They consider various factors, so it's always a good idea to double-check you meet all the entry requirements and have all the necessary documents handy.`,
+    },
+    {
+      id: 4,
+      title: `Oops, Application Mistake! Here's What to Do.`,
+      des: `We've all been there! If you spot an error after submitting your application, reach out to our customer support team ASAP. They'll do their best to help, but if the application's already been reviewed and sent to the government, changes might be tricky.`,
+    },
+  ],
+};
