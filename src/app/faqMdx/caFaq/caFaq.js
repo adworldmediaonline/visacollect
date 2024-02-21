ef25c223d73cbfc117289b66db145e625b4d293e
fastestsunil <sunil141292@gmail.com>
2024-02-21 18:02:00 +0530

    canada inside in directiry

---
+++ src/app/faqMdx/caFaq/caFaq.js
@@ -0,0 +1,51 @@
+export const caFaq = [
+  {
+    id: 1,
+    title: 'How much does India Visa costs?',
+    content:
+      'The cost of an India visa depends on the type of visa and your nationality. For Canadian citizens, the fee for an e-Tourist Visa is $53.97 CAD or ₹3318.93 INR | $107.94 CAD or ₹6637.85, plus a 2.5% bank charge. For a regular tourist visa, the fee is $100 CAD or ₹6149.57 INR, plus a service charge of $7.40 CAD or ₹455.07 INR. You can pay the visa fee online using your credit or debit card. |',
+  },
+  {
+    id: 2,
+    title: 'Can I apply for India e Visa on arrival from Canada?',
+    content:
+      'No, you cannot apply for an India e Visa on arrival from Canada. You must apply for an e Visa in advance, either online or at an Indian embassy or consulate. The e Visa on arrival facility has been suspended due to the COVID-19 pandemic.',
+  },
+  {
+    id: 3,
+    title: 'What documents are needed to apply for India e Visa?',
+    // mdxContent: <UsFaq3MDX />,
+    content:
+      'To apply for an India e Visa, you will need a valid Canadian passport, a recent photo, an email address, a credit or debit card, and some additional documents depending on the type of e Visa and your',
+  },
+  {
+    id: 4,
+    title: 'Do I need a SIM card for my phone?',
+    content: `Long-term visas like Business, Student, and Research require additional documents and specific approvals.`,
+    // mdxContent: <UkFaq4MDX />,
+  },
+  {
+    id: 5,
+    title: 'Can I drive in India?',
+    content: `You can with an International Driving Permit (IDP) and valid Canadian driver's license. Traffic rules and road conditions differ, so drive cautiously.`,
+    // mdxContent: <UkFaq4MDX />,
+  },
+  {
+    id: 6,
+    title: 'What are some cultural norms to be aware of?',
+    content: `Respect religious sites, dress modestly, and remove shoes when entering temples or homes. Public displays of affection may be frowned upon.`,
+    // mdxContent: <UkFaq4MDX />,
+  },
+  {
+    id: 7,
+    title: 'What are some cultural norms to be aware of?',
+    content: `Respect religious sites, dress modestly, and remove shoes when entering temples or homes. Public displays of affection may be frowned upon.`,
+    // mdxContent: <UkFaq4MDX />,
+  },
+  {
+    id: 8,
+    title: 'How can I learn basic Hindi phrases?',
+    content: `Respect religious sites, dress modestly, and remove shoes when entering temples or homes. Public displays of affection may be frowned upon.`,
+    // mdxContent: <UkFaq4MDX />,
+  },
+];
