c8b7379bd3fabcc7130a1494f97349c5cf0d5349
fastestsunil <sunil141292@gmail.com>
2024-02-28 13:09:38 +0530

    error resovle

--- src/app/(visaCountries)/turkey/us/_homePage/SectionHeading.js
+++
@@ -1,8 +0,0 @@
-export default function SectionHeading({ heading }) {
-  return (
-    <div
-      className="flex justify-start pt-10 [&_h1]:text-primary [&_h1]:text-2xl font-medium [&_h1]:md:text-3xl [&_h2]:text-primary [&_h2]:text-2xl [&_h2]:md:text-3xl [&_h3]:text-primary [&_h3]:text-2xl [&_h3]:md:text-3xl [&_h4]:text-primary [&_h4]:text-2xl [&_h4]:md:text-3xl [&_h5]:text-primary [&_h5]:text-2xl [&_h5]:md:text-3xl [&_h6]:text-primary [&_h6]:text-2xl [&_h6]:md:text-3xl"
-      dangerouslySetInnerHTML={{ __html: `${heading ?? ''}` }}
-    />
-  );
-}
