abd5c2647013a09734dea9568dd5024a7f724e4a
fastestsunil <sunil141292@gmail.com>
2024-03-01 11:13:56 +0530

    maindirectory added

---
+++ src/app/(visaCountries)/mainDirectoryHomePagesBlog/india/index.js
@@ -0,0 +1,9 @@
+// india visa
+export { default as topThingsToDoInIndiaForFun } from './top-things to-do-in-india-for-fun.mdx';
+export { default as bestThingsToDoInIndiaForAustraliaCitizens } from './australia/best-things-to-do-in-india-for-australia-citizens.mdx';
+export { default as applyIndiaTouristVisaForCanadianCitizens } from './canada/apply-india-tourist-visa-for-canadian-citizens.mdx';
+export { default as indianVisaForEmiratisCitizens } from './uae/indian-visa-for-emiratis-citizens.mdx';
+export { default as applyIndiaEVisaFromUK } from './uk/apply-india-eVisa-from-uk.mdx';
+export { default as indianTouristVisaForUKCitizens } from './uk/indian-tourist-visa-for-uk-citizens.mdx';
+export { default as howToGetYourIndiaVisaForUSCitizens } from './us/how-to-get-your-india-visa-for-us-citizens.mdx';
+export { default as stepsToApplyYourIndiaEVisaFromUSA } from './us/steps-to-apply-your-india-e-visa-from-usa.mdx';
