4f686cc2b3d5e28fbcae77c67f8615e0be424034
TusharDC <tushar@digitalcappuccino.com>
2024-03-01 11:37:20 +0530

    error resolve

--- src/components/ui/link-text.js
+++
@@ -1,8 +0,0 @@
-export const LinkText = ({ text, href }) => (
-    <Link
-        className="inline-flex items-center justify-center gap-1 text-lg font-semibold text-primary"
-        href={href}
-    >
-        {text} <FaLongArrowAltRight />
-    </Link>
-);
