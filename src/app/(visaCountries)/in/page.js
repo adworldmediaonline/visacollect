8af76aeb843d2ee66715f4ab586ae1610093a6d6
fastestsunil <sunil141292@gmail.com>
2024-03-01 15:31:27 +0530

    data updated

--- src/app/(visaCountries)/in/page.js
+++ src/app/(visaCountries)/in/page.js
@@ -9,44 +9,8 @@ import BlogSlider from '@/components/commonWebsiteComponents/BlogSlider';
 import Faq from '@/components/commonWebsiteComponents/Faq';
 import { learnMoreSectionDataIndia } from '@/constant/countryHomePageData/india';
 import Banner2 from '@/components/ui/Banner2';
-import { indiaMDData } from '../mainDirectoryData/mainDirectoryData';
+import { indiaMDData } from '../mainDirectoryData/indiaMDData';
 
-const blogs = [
-  {
-    title:
-      'Everything You Need to Know to Stress-Free Travel Planning for Any Trip',
-    description:
-      'Discover the simplicity of securing your UK to Australia eVisa with ease for a seamless travel experience.',
-    slug: '/blog/tips-for-stress-free-travel',
-    linkText: 'Read More',
-    img: 'https://dummyimage.com/720x400',
-  },
-  {
-    title: 'Happiest City Index - Best Cities to Travel in 2024',
-    description:
-      'Experience joy in the top cities of 2024! Discover the Happiest City Index for the best travel destinations. Happiness awaits in every corner.',
-    slug: '/blog/best-cities-to-travel-in-2024',
-    linkText: 'Read More',
-    img: 'https://dummyimage.com/720x400',
-  },
-  {
-    title:
-      'Top Best Free Game-Changing Social Media Tools and Strategies For Travel Agents',
-    description:
-      'Experience joy in the top cities of 2024! Discover the Happiest City Index for the best travel destinations. Happiness awaits in every corner.',
-    slug: '/blog/strategies-for-travel-agents',
-    linkText: 'Read More',
-    img: 'https://dummyimage.com/720x400',
-  },
-  {
-    title: 'How to Use Social Media to Attract More Clients as a Travel Agent',
-    description:
-      'Experience joy in the top cities of 2024! Discover the Happiest City Index for the best travel destinations. Happiness awaits in every corner.',
-    slug: '/blog/social-media-travel-agents-for-visa',
-    linkText: 'Read More',
-    img: 'https://dummyimage.com/720x400',
-  },
-];
 export default async function Page() {
   return (
     <div>
@@ -63,9 +27,12 @@ export default async function Page() {
         <MainWrapper>{indiaMDData?.pageContent}</MainWrapper>
         <AsideWrapper>
           <ul className="flex flex-col gap-3">
-            {blogs?.map(blog => (
-              <li key={blog.title}>
-                <AsideBlogCard slug={blog.slug} title={blog.title} />
+            {indiaMDData?.blogs?.map(blog => (
+              <li key={blog?.title}>
+                <AsideBlogCard
+                  slug={blog?.href}
+                  title={blog?.metadata?.title ?? 'Title not found'}
+                />
               </li>
             ))}
           </ul>
@@ -76,10 +43,10 @@ export default async function Page() {
       </div>
 
       <div>
-        <Faq faqData={learnMoreSectionDataIndia?.faqData} />
+        <Faq faqData={indiaMDData?.faq ?? []} />
       </div>
       <PageReview applyLink="/in/visa/step-one" />
-      <BlogSlider blogs={blogs} />
+      <BlogSlider blogs={indiaMDData?.blogs ?? []} />
     </div>
   );
 }
