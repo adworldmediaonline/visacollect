a6dcc164a2de5b420d41bb5cc0a7adcef57c7998
TusharDC <tushar@digitalcappuccino.com>
2024-03-11 17:40:58 +0530

    content optimisation

--- src/app/(blogContent)/blog/page.js
+++ src/app/(blogContent)/blog/page.js
@@ -1,17 +1,17 @@
-import HomePageTitle from '@/components/common/countryHomePage/HomePageTitle';
-import Banner3 from '@/components/ui/Banner3';
-import PageWrapper from './components/PageWrapper';
-import MainWrapper from './components/MainWrapper';
+import HomePageTitle from "@/components/common/countryHomePage/HomePageTitle";
+import Banner3 from "@/components/ui/Banner3";
+import PageWrapper from "./components/PageWrapper";
+import MainWrapper from "./components/MainWrapper";
 
 export default function BlogPage() {
-  return (
-    <>
-      <Banner3 breadcrumb="blog" className="pb-0" />
-      <PageWrapper>
-        <MainWrapper>
-          <HomePageTitle pageTitle="Take a journey through visa-related blogs to uncover the recent trends" />
-        </MainWrapper>
-      </PageWrapper>
-    </>
-  );
+    return (
+        <>
+            <Banner3 breadcrumb="blog" className="pb-0" />
+            <PageWrapper>
+                <MainWrapper>
+                    <HomePageTitle pageTitle="Take a journey through visa-related blogs to uncover the recent trends" />
+                </MainWrapper>
+            </PageWrapper>
+        </>
+    );
 }
