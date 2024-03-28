0a971d1232c370e8cea198c9b66e369fedc7b207
TusharDC <tushar@digitalcappuccino.com>
2024-03-28 16:09:08 +0530

    blogts alt text added

--- src/app/(visaCountries)/mainDirectoryData/turkeyMDData.js
+++ src/app/(visaCountries)/mainDirectoryData/turkeyMDData.js
@@ -1,15 +1,16 @@
-import { TurkeyHomePage } from '../mainDirectoryHomePages';
-import { turkeyMDHomePageFaq } from '../mainDirectoryHomePagesFaq/turkeyMDHomePageFaq';
+import { TurkeyHomePage } from "../mainDirectoryHomePages";
+import { turkeyMDHomePageFaq } from "../mainDirectoryHomePagesFaq/turkeyMDHomePageFaq";
 
 // MD => mainDirectory
-const turkeyMDBlogBase = '/tr/blog';
+const turkeyMDBlogBase = "/tr/blog";
 export const turkeyMDData = {
-  breadcrumb: 'TR',
-  code: 'tr',
-  pageTitle: 'Turkey Visa Entry requirements and travel information for Turkey',
-  pageDescription: null,
-  pageContent: <TurkeyHomePage />,
-  applyNow: '/tr/application',
-  faq: turkeyMDHomePageFaq,
-  blogs: [],
+    breadcrumb: "TR",
+    code: "tr",
+    pageTitle:
+        "Turkey Visa Entry requirements and travel information for Turkey",
+    pageDescription: null,
+    pageContent: <TurkeyHomePage />,
+    applyNow: "/tr/application",
+    faq: turkeyMDHomePageFaq,
+    blogs: [],
 };
