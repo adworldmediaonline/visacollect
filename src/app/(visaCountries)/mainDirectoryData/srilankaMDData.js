5aa4af3c0704750fbb4ddb0f9385669b5a3c75ea
TusharDC <tushar@digitalcappuccino.com>
2024-05-03 18:00:23 +0530

    main directory updated

--- src/app/(visaCountries)/mainDirectoryData/srilankaMDData.js
+++ src/app/(visaCountries)/mainDirectoryData/srilankaMDData.js
@@ -1,43 +1,42 @@
-import { SrilankaHomePage } from '../mainDirectoryHomePages';
-import { sriLankanVisaRequirementsForCanadianCitizens } from '../mainDirectoryHomePagesBlog/images/blogImages';
-import { SriLankanVisaRequirementsForCanadianCitizens } from '../mainDirectoryHomePagesBlog/srilanka';
-import { srilankaMDHomePageFaq } from '../mainDirectoryHomePagesFaq/srilankaMDHomePageFaq';
+import { SrilankaHomePage } from "../mainDirectoryHomePages";
+import { sriLankanVisaRequirementsForCanadianCitizens } from "../mainDirectoryHomePagesBlog/images/blogImages";
+import { SriLankanVisaRequirementsForCanadianCitizens } from "../mainDirectoryHomePagesBlog/srilanka";
+import { srilankaMDHomePageFaq } from "../mainDirectoryHomePagesFaq/srilankaMDHomePageFaq";
 
 // MD => mainDirectory
-const srilankaMDBlogBase = '/lk/blog';
+const srilankaMDBlogBase = "/lk/blog";
 export const srilankaMDData = {
-  breadcrumb: 'LK',
-  code: 'lk',
-  pageTitle:
-    'Srilanka Visa Entry requirements and travel information for Srilanka',
-  pageDescription: null,
-  pageContent: <SrilankaHomePage />,
-  applyNow: '/lk/slvisa/tourist-eta/apply-individual',
-  faq: srilankaMDHomePageFaq,
-  blogs: [
-    {
-      metadata: {
-        title: `Sri Lanka Visa Requirements for Canadian Citizens`,
-        description: ``,
-        metadataBase: new URL('https://visacollect.com'),
+    breadcrumb: "LK",
+    code: "lk",
+    pageTitle:
+        "Srilanka Visa Entry requirements and travel information for Srilanka",
+    pageDescription: null,
+    pageContent: <SrilankaHomePage />,
+    applyNow: "/lk/slvisa/tourist-eta/apply-individual",
+    faq: srilankaMDHomePageFaq,
+    blogs: [
+        {
+            metadata: {
+                title: `Sri Lanka Visa Requirements for Canadian Citizens`,
+                description: `Unlocking Sri Lanka for Canadians: This blog explains everything Canadians need to know about obtaining an ETA for a smooth trip to Sri Lanka. `,
+                metadataBase: new URL("https://visacollect.com"),
 
-        alternates: {
-          canonical: `${srilankaMDBlogBase}/sri-lankan-visa-requirements-for-canadian-citizens`,
-        },
+                alternates: {
+                    canonical: `${srilankaMDBlogBase}/sri-lankan-visa-requirements-for-canadian-citizens`,
+                },
 
-        openGraph: {
-          url: `${srilankaMDBlogBase}/sri-lankan-visa-requirements-for-canadian-citizens`,
+                openGraph: {
+                    url: `${srilankaMDBlogBase}/sri-lankan-visa-requirements-for-canadian-citizens`,
+                },
+            },
+            slug: "sri-lankan-visa-requirements-for-canadian-citizens",
+            pageTitle: "",
+            content: <SriLankanVisaRequirementsForCanadianCitizens />,
+            img: sriLankanVisaRequirementsForCanadianCitizens,
+            alt: "Sri Lankan visa requirements for Canadian citizens | Visacollect",
+            imgUrl: "/assets/images/srilanka/blog/sri-lankan-visa-requirements-for-canadian-citizens.webp",
+            linkText: "Read more",
+            href: `${srilankaMDBlogBase}/sri-lankan-visa-requirements-for-canadian-citizens`,
         },
-      },
-      slug: 'sri-lankan-visa-requirements-for-canadian-citizens',
-      pageTitle: '',
-      content: <SriLankanVisaRequirementsForCanadianCitizens />,
-      img: sriLankanVisaRequirementsForCanadianCitizens,
-      alt: '',
-      imgUrl:
-        '/assets/images/srilanka/blog/sri-lankan-visa-requirements-for-canadian-citizens.webp',
-      linkText: 'Read more',
-      href: `${srilankaMDBlogBase}/sri-lankan-visa-requirements-for-canadian-citizens`,
-    },
-  ],
+    ],
 };
