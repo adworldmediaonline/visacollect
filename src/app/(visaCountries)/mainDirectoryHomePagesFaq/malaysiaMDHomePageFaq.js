a91f2789d549be4c81558b3ddd53134473c1fce4
TusharDC <tushar@digitalcappuccino.com>
2024-05-11 02:13:01 +0530

    sg om tr my content faq updated

--- src/app/(visaCountries)/mainDirectoryHomePagesFaq/malaysiaMDHomePageFaq.js
+++ src/app/(visaCountries)/mainDirectoryHomePagesFaq/malaysiaMDHomePageFaq.js
@@ -1,79 +1,52 @@
 export const malaysiaMDHomePageFaq = [
-  {
-    id: 1,
-    title: 'How long does it take to obtain a Visa?',
-    des: 'It takes 48 to 72 hours for processing, excluding weekends, public holidays, and specific country holidays. Please note that processing time may be affected by unforeseen events.',
-  },
-  {
-    id: 2,
-    title: 'eVISA printing format?',
-    des: 'Your Online eVISA to Malaysia should be printed in an A4 format either in colour or black and white.',
-  },
-  {
-    id: 3,
-    title: 'Who can apply for Malaysia Visa online?',
-    des: 'eVisa is available for the citizens of China, India, Sri Lanka, Nepal, Myanmar, Bangladesh, Pakistan, Bhutan, Serbia, Montenegro, and more.',
-  },
-  {
-    id: 4,
-    title: 'What types of eVisas are offered from Malaysia?',
-    des: 'Single-Entry Visa (SEV) for specific nationals, allowing a maximum stay of 30 days with no extensions. Multiple-Entry Visa (MEV) & Single-Entry Visa (SEV) for Indian Nationals, with a maximum stay of 30 days for each visit with no extensions.',
-  },
-  {
-    id: 5,
-    title: 'Minimum passport blank pages prior to entry into Malaysia?',
-    des: 'The minimum required blank passport pages are three ( = 3).',
-  },
-  {
-    id: 6,
-    title:
-      'How to provide accommodation documents if staying at a friend or relative’s place?',
-    des: 'Submit a cover letter stating your name, passport details, address in Malaysia, and your friend’s or relative’s details merged into one file in the Accommodation section.',
-  },
-  {
-    id: 7,
-    title: 'How to apply for a Malaysian business visa online?',
-    des: 'eVisa is only for tourism purposes. Apply for a business visa to Malaysia at the High Commission, Consulate, or Embassy of Malaysia nearest to you.',
-  },
-  {
-    id: 8,
-    title: 'How to upload multiple documents for an online eVisa?',
-    des: 'Merge all documents into one file for each category (e.g., hotel booking, air tickets) before uploading.',
-  },
-  {
-    id: 9,
-    title: 'What to fill in the “Address in Malaysia” for transit stays?',
-    des: "Provide the address of Malaysia's International Airport and submit a cover letter explaining your transit.",
-  },
-  {
-    id: 10,
-    title: 'Can I use a digital visa (mobile or laptop) to enter Malaysia?',
-    des: 'Yes, both printed and digital visas are accepted.',
-  },
-  {
-    id: 11,
-    title: 'What are the requirements for eVisa?',
-    des: "Ensure valid passport, recent photo, confirmed return flight booking, birth certificate for minors, proof of accommodation, and other documentation depending on the applicant's country.",
-  },
-  {
-    id: 12,
-    title: 'How long is my Malaysian eVisa valid for?',
-    des: 'eVisa is valid for 3 months, allowing a maximum stay of 30 days per visit in Malaysia.',
-  },
-  {
-    id: 13,
-    title: 'How many days can I stay in Malaysia with an eVisa?',
-    des: 'You are allowed to stay for a maximum of 30 days per entry.',
-  },
-  {
-    id: 14,
-    title:
-      'How long before my travel date should I apply online for a Malaysian eVisa?',
-    des: 'Apply at least 2 weeks prior to departure to Malaysia.',
-  },
-  {
-    id: 15,
-    title: 'Can I apply for Visa in Malaysia?',
-    des: 'No, eVisa must be obtained prior to entry into Malaysia.',
-  },
+    {
+        id: 1,
+        title: "At what time of year is Malaysia the best to visit?",
+        des: "Most of the time, March through early October is the best time to visit Malaysia because the weather is dry, especially on the west coast, where Kuala Lumpur and Penang are situated.",
+    },
+    {
+        id: 2,
+        title: "Which Malaysian sites really should not be missed?",
+        des: "The Petronas Twin Towers in Kuala Lumpur, the ancient city of George Town in Penang, the lush Cameron Highlands, and the beautiful islands of Langkawi and Perhentian are some of the most popular places to visit.",
+    },
+    {
+        id: 3,
+        title: "I'm going to Malaysia; what should I pack?",
+        des: "A good sunscreen, comfortable walking shoes, a jacket or umbrella for unexpected showers and lightweight clothing appropriate for hot and humid weather are musts. Remember to bring adapters for the Type G, 240V plugs in Malaysia.",
+    },
+    {
+        id: 4,
+        title: "Is there anything cultural that I should know about Malaysia?",
+        des: "With major Malay, Chinese, and Indian populations, Malaysia is a multicultural nation. It's crucial to honour regional traditions and religious rites. For example, it is normal to take off your shoes before entering homes and places of worship.",
+    },
+    {
+        id: 5,
+        title: "Where can I change money and what currency is used in Malaysia?",
+        des: "Used currency is the Malaysian Ringgit (MYR). All over the nation, banks, airports, and licenced money changers accept exchanges of currency. A lot of ATMs are available to get local cash.",
+    },
+    {
+        id: 6,
+        title: "How easy is it to travel about Malaysia?",
+        des: "Malaysia boasts a highly developed transport system. Buses, trains and taxis are among the many and reasonably priced forms of public transportation in cities. There are local flights, buses, and trains for travel between cities.",
+    },
+    {
+        id: 7,
+        title: "How should I expect to eat in Malaysia?",
+        des: "As befits its multinational population, Malaysian food is highly varied. Staples include Roti Canai, Satay, and Nasi Lemak. Every ethnic group has its own flavours, and you have to try street food. Every ethnic group has its own specialties, and you really must sample the street food.",
+    },
+    {
+        id: 8,
+        title: "What conditions must I meet to get a Malaysian visa?",
+        des: "For the most part, visitors to Malaysia require a visa, which varies according on nationality. Usually, you need a legal passport, proof that you have enough money, confirmed return tickets, and hotel reservations.",
+    },
+    {
+        id: 9,
+        title: "Are visitors secure in Malaysia?",
+        des: "Most people think Malaysia is safe for tourists. But, as with any travel place, it's best to follow normal safety rules, stay away from less touristy areas at night, and keep an eye on your belongings.",
+    },
+    {
+        id: 10,
+        title: "Can I use Malaysian mobile phones?",
+        des: "Good cellphone coverage in Malaysia means that many tourists decide to get a local SIM card for calls and data. Buy a SIM only after making sure your phone is unlocked.",
+    },
 ];
