3dc13de54cfd8a1841a55fef9e51c2fd11b94786
TusharDC <tushar@digitalcappuccino.com>
2024-05-18 13:45:07 +0530

    id jp content updated

--- src/app/(visaCountries)/mainDirectoryHomePagesFaq/indonesiaMDHomePageFaq.js
+++ src/app/(visaCountries)/mainDirectoryHomePagesFaq/indonesiaMDHomePageFaq.js
@@ -1,122 +1,53 @@
 export const indonesiaMDHomePageFaq = [
-  {
-    id: 1,
-    title: 'How long does it take to process an Indonesian eVISA?',
-    des: `Processing typically takes about 3 to 10 business days, depending on the type of visa.`,
-  },
-  {
-    id: 2,
-    title: `What documents do I need to apply for an Indonesian eVISA?`,
-    des: `You'll need your passport, visa application form, a photo, and other documents based on the visa type.`,
-  },
-  {
-    id: 3,
-    title: `How do I apply for an Indonesia visa online? `,
-    des: `You can apply for an Indonesia visa online by following these steps:`,
-    extraContent: [
-      {
-        text: `Visit the official website of the Directorate General of Immigration of Indonesia and create an account with your email and password.  `,
-      },
-      {
-        text: `<strong>Or</strong>`,
-      },
-      {
-        text: `- Fill out the online application form with your personal and travel information, and upload the required documents, such as your passport, photo, and travel itinerary.`,
-      },
-      {
-        text: `- Pay the visa fee of USD 15 using your credit or debit card.`,
-      },
-      {
-        text: `- Receive your visa approval letter by email within three working days.`,
-      },
-      {
-        text: `- Print out your visa approval letter and bring it with you when you travel to Indonesia.`,
-      },
-    ],
-  },
-  {
-    id: 4,
-    title: `I made a mistake on my application. What should I do?`,
-    des: `Contact the Indonesian embassy or consulate promptly to rectify any errors.`,
-  },
-  {
-    id: 5,
-    title: `My visa application was denied. Can I apply again?`,
-    des: `Yes, you can reapply after understanding the reasons for the denial and rectifying them.`,
-  },
-  {
-    id: 6,
-    title: `Do I need to show proof of accommodation and flight bookings when applying for a Indonesiaese visa?`,
-    des: `Yes, providing these details may be a requirement for your application.`,
-  },
-  {
-    id: 7,
-    title: `Can I extend my stay in Indonesia beyond the period granted by my visa?`,
-    des: `Extensions are possible in certain circumstances, but generally, you should plan to leave Indonesia before your visa expires.`,
-  },
-  {
-    id: 8,
-    title: `How long is an Indonesia visa online valid for? `,
-    des: `An Indonesia visa online is valid for 90 days from the date of issue, as stated on the visa approval letter. However, the duration of stay in Indonesia is limited to 30 days from the date of arrival, and cannot be extended or converted into another type of visa or stay permit. You must leave Indonesia before your visa expires, or you may face penalties or deportation.`,
-  },
-  {
-    id: 9,
-    title: `What should I do if I lose or damage my Indonesia visa online approval letter? `,
-    des: `If you lose or damage your Indonesia visa online approval letter, you should print out a new copy from your email or from your online account. You should always carry a printed copy of your visa approval letter with you when you travel to Indonesia, as you will need to present it at the immigration checkpoint along with your passport and other documents.`,
-  },
-  {
-    id: 10,
-    title: `Can I apply for an Indonesia visa online for multiple people?`,
-    des: `Yes, you can apply for an Indonesia visa online for multiple people, as long as they meet the eligibility criteria and have the required documents. You can create a group application online and add up to 20 applicants in one submission. You will need to fill out the application form and upload the documents for each applicant individually, and pay the visa fee for the whole group at once.`,
-  },
-  {
-    id: 11,
-    title: `Can I change my travel plans after I receive my Indonesia visa online? `,
-    des: `You can change your travel plans after you receive your Indonesia visa online, as long as you do not exceed the validity period and the duration of stay of your visa. However, you should note that your visa is linked to your passport number, and you cannot use it with a different passport. If you change your passport for any reason, you will need to apply for a new visa with your new passport details.`,
-  },
-  {
-    id: 12,
-    title: `What are the health requirements for entering Indonesia with an e-Visa? `,
-    des: `The health requirements for entering Indonesia with an e-Visa are:`,
-    extraContent: [
-      {
-        text: `- You must have a negative COVID-19 PCR test result issued within 72 hours before your departure to Indonesia, and present it to the airline staff and the immigration officer upon arrival.`,
-      },
-      {
-        text: `- You must fill out an electronic health alert card (e-HAC) online before your departure to Indonesia, and show the QR code to the health officer upon arrival.`,
-      },
-      {
-        text: `- You must undergo a health screening and a COVID-19 PCR test upon arrival in Indonesia, and wait for the result at a designated hotel or facility. If the result is negative, you can continue your trip. If the result is positive, you will be transferred to a hospital or a quarantine facility for further treatment.`,
-      },
-      {
-        text: `- You must follow the health protocols in Indonesia, such as wearing a mask, social distancing, and washing your hands frequently.`,
-      },
-    ],
-  },
-  {
-    id: 13,
-    title: `What are the consequences of overstaying or violating the conditions of my Indonesia visa online?`,
-    des: `If you overstay or violate the conditions of your Indonesia visa online, you may face serious consequences, such as:`,
-    extraContent: [
-      {
-        text: `- Fines of up to IDR 1,000,000 per day of overstay, or up to IDR 500,000,000 for violating the visa conditions`,
-      },
-      {
-        text: `- Detention or imprisonment for up to five years`,
-      },
-      {
-        text: `- Deportation or blacklisting from entering Indonesia again`,
-      },
-    ],
-  },
-  {
-    id: 14,
-    title: `Can I Extend My Stay in Indonesia with an eVisa?`,
-    des: `The Indonesia eVisa is valid for a stay of up to 60 days. If you wish to extend your stay, you will need to apply for a visa extension at the local immigration office in Indonesia. Please note that the decision to grant an extension is at the discretion of the Indonesian immigration authorities.`,
-  },
-  {
-    id: 15,
-    title: `What If My eVisa Application Is Denied?`,
-    des: `If your eVisa application is denied, you will receive a notification explaining the reason for the denial. You may then choose to address the issues mentioned and reapply, or you can apply for a traditional visa at the Indonesian embassy or consulate in your jurisdiction. It's important to understand the reason for the denial to improve your chances of approval in the future.`,
-  },
+    {
+        id: 1,
+        title: "When processing an application for an Indonesian visa takes how long?",
+        des: `The kind of visa and nationality of the applicant can affect how long it takes to process an application for an Indonesian visa. That usually takes five to ten business days. It is advised to apply much in advance of the dates you intend to travel.`,
+    },
+    {
+        id: 2,
+        title: `Can I stay in Indonesia longer on a tourist visa?`,
+        des: `Extensions of Indonesian tourist visas are usually not possible. On arrival visas, for example, can be renewed once for an extra thirty days. For further information, see the conditions of your visa or get in touch with the Indonesian immigration authorities.`,
+    },
+    {
+        id: 3,
+        title: `Which photos must be included with an application for an Indonesian visa?`,
+        des: `The picture has to be a current colour one taken on a white backdrop. It needs to measure 3.5 by 4.5 centimetres. The candidate should be smiling neutrally and the picture should be clear, free of reflections or shadows.`,
+    },
+    {
+        id: 4,
+        title: `I'm passing through Indonesia; do I still need a visa?`,
+        des: `Generally speaking, you don't require a visa if you are passing through Indonesia and don't intend to depart the airport transit area. You could need a transit visa, though, if you want to leave the airport or have a layover longer than 24 hours.`,
+    },
+    {
+        id: 5,
+        title: `How should I get around in Indonesia?`,
+        des: `Indonesia boasts a sophisticated network of domestic airlines, ferries, buses, and railroads. Shortest and most practical for long-distance travel are domestic flights. Travelling between islands often involves boats and ferries. Public buses, ride-sharing services, and taxis are all readily accessible inside cities.`,
+    },
+    {
+        id: 6,
+        title: `Does one have to meet any health standards before visiting Indonesia?`,
+        des: `It is advised to be current on normal vaccinations including measles, mumps, rubella, and diphtheria even though they are not required for admission into Indonesia. The places you intend to visit may also require vaccinations against typhoid, malaria, and hepatitis A. See your doctor for specific guidance.
+`,
+    },
+    {
+        id: 7,
+        title: `Should my visa application to Indonesia be turned down, what should I do?`,
+        des: `Should your visa application be turned down, you should normally get a letter outlining the grounds for the rejection. Should you be able to resolve the problems listed in the denial notification, you may reapply. Consulting with visa specialists is a good idea to make sure your next application is precise and thorough.`,
+    },
+    {
+        id: 8,
+        title: `When visiting Indonesia, what customs should one be mindful of?`,
+        des: `Indonesians respect and are courteous people. A smile and a little bow are usual greetings. If you enter a mosque or temple, take off your shoes and dress modest. Generally speaking, it is not a good idea to show affection in public, hence it is crucial to honour regional traditions.`,
+    },
+    {
+        id: 9,
+        title: `What entrance requirements apply to kids visiting Indonesia? `,
+        des: `Youngsters entering Indonesia need to have their own passport and visa. Bring a notarized letter of permission from the parent or legal guardian who is not travelling if you are travelling with just one. Make sure that any children' travel documents are current and complete.`,
+    },
+    {
+        id: 10,
+        title: `Can a tourist visa holder work in Indonesia?`,
+        des: `Not with a tourist visa can you work in Indonesia. Depending on the type of job you plan to have, you will need to apply for either a work visa or a business visa. Obtaining the right visa is crucial to stay out of legal hot waters.`,
+    },
 ];
