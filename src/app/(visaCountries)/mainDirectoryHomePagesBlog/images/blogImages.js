5f7389d9e6e200a78cdb58a5ce5b9bb7c727908d
fastestsunil <sunil141292@gmail.com>
2024-07-19 10:37:51 +0530

    redirects to new url and essenails indian visa

--- src/app/(visaCountries)/mainDirectoryHomePagesBlog/images/blogImages.js
+++ src/app/(visaCountries)/mainDirectoryHomePagesBlog/images/blogImages.js
@@ -1,45 +1,48 @@
-import imageNotFound from '/public/assets/images/blog/image-not-found.jpg';
-import topThingsToDoInIndiaForFunAustraliaCitizensImg from '/public/assets/images/blog/indiaVisa/top-things-to-do-in-india-for-fun.webp';
-import bestThingsToDoInIndiaForAustraliaCitizensImg from '/public/assets/images/blog/indiaVisa/best-things-to-do-in-india-for-australia-citizens.webp';
-import applyIndiaTouristVisaForCanadianCitizensImg from '/public/assets/images/blog/indiaVisa/apply-india-tourist-visa-for-canadian-citizens.webp';
-import applyIndiaEvisaFromUKImg from '/public/assets/images/blog/indiaVisa/apply-india-eVisa-from-uk.webp';
-import indianTouristVisaForUkCitizensImg from '/public/assets/images/blog/indiaVisa/indian-tourist-visa-for-uk-citizens.webp';
-import howToGetYourIndiaTouristVisaForUsCitizensImg from '/public/assets/images/blog/indiaVisa/how-to-get-your-india-tourist-visa-for-us-citizens.webp';
-import indianVisaForEmiratisCitizensImg from '/public/assets/images/blog/indiaVisa/indian-visa-for-emiratis-citizens.webp';
-import stepsToApplyYourIndiaEvisaFromUsaImg from '/public/assets/images/blog/indiaVisa/steps-to-apply-your-india-e-visa-from-usa.webp';
+import australiaVisaForUsCitizensImg from '/public/assets/images/blog/australiaVisa/australia-visa-for-us-citizens.webp';
 import australiaVisaRequirementsForSingaporeCitizensImg from '/public/assets/images/blog/australiaVisa/australia-visa-requirements-for-singapore-citizens.webp';
 import australianEtaForUkCitizensImg from '/public/assets/images/blog/australiaVisa/australian-eta-for-uk-citizens.webp';
-import australiaVisaForUsCitizensImg from '/public/assets/images/blog/australiaVisa/australia-visa-for-us-citizens.webp';
+import imageNotFound from '/public/assets/images/blog/image-not-found.jpg';
+import applyIndiaEvisaFromUKImg from '/public/assets/images/blog/indiaVisa/apply-india-eVisa-from-uk.webp';
+import applyIndiaTouristVisaForCanadianCitizensImg from '/public/assets/images/blog/indiaVisa/apply-india-tourist-visa-for-canadian-citizens.webp';
+import bestThingsToDoInIndiaForAustraliaCitizensImg from '/public/assets/images/blog/indiaVisa/best-things-to-do-in-india-for-australia-citizens.webp';
+import howToGetYourIndiaTouristVisaForUsCitizensImg from '/public/assets/images/blog/indiaVisa/how-to-get-your-india-tourist-visa-for-us-citizens.webp';
+import indianTouristVisaForUkCitizensImg from '/public/assets/images/blog/indiaVisa/indian-tourist-visa-for-uk-citizens.webp';
+import indianVisaForEmiratisCitizensImg from '/public/assets/images/blog/indiaVisa/indian-visa-for-emiratis-citizens.webp';
+import stepsToApplyYourIndiaEvisaFromUsaImg from '/public/assets/images/blog/indiaVisa/steps-to-apply-your-india-e-visa-from-usa.webp';
+import topThingsToDoInIndiaForFunAustraliaCitizensImg from '/public/assets/images/blog/indiaVisa/top-things-to-do-in-india-for-fun.webp';
 import oneYearMultiEntryNonImmigrantVisaUkToThailandImg from '/public/assets/images/blog/thailandVisa/1-year-multi-entry-non-immigrant-visa.webp.webp';
 import completeThailandVisaGuideForUkCitizensImg from '/public/assets/images/blog/thailandVisa/complete-thailand-visa-guide-for-uk-citizens.webp';
 import howToApplyForThailandVisaFromUkImg from '/public/assets/images/blog/thailandVisa/how-to-apply-for-thailand-visa-from-uk.webp';
 import travelToThailandFromUkImg from '/public/assets/images/blog/thailandVisa/travel-to-thailand-from-uk.webp';
 import sriLankanVisaRequirementsForCanadianCitizens from '/public/assets/images/srilanka/blog/sri-lankan-visa-requirements-for-canadian-citizens.webp';
 
-import indiaEVisaApplyOnline from '/public/assets/images/generalBlog/india-evisa-apply-online.webp';
 import moroccoVisaEligibilityImg from '/public/assets/images/blog/moroccoVisa/morocco-visa-eligibility-application-and-requirements.webp';
 import yourCompleteGuideImg from '/public/assets/images/blog/moroccoVisa/your-complete-guide-to-obtaining-a-visa-in-morocco.webp';
 import howToGetTurkeyVisa from '/public/assets/images/blog/turkeyVisa/how-to-get-a-turkey-visa-after-a-refusal.webp';
+import indiaEVisaApplyOnline from '/public/assets/images/generalBlog/india-evisa-apply-online.webp';
+
+import essentialTipsForIndianBusinessVisa from '/public/assets/images/blog/indiaVisa/essential-tips-for-indian-business-visa.webp';
 export {
-  moroccoVisaEligibilityImg,
-  yourCompleteGuideImg,
-  howToGetTurkeyVisa,
-  imageNotFound,
-  topThingsToDoInIndiaForFunAustraliaCitizensImg,
-  bestThingsToDoInIndiaForAustraliaCitizensImg,
-  applyIndiaTouristVisaForCanadianCitizensImg,
   applyIndiaEvisaFromUKImg,
-  indianTouristVisaForUkCitizensImg,
-  howToGetYourIndiaTouristVisaForUsCitizensImg,
-  indianVisaForEmiratisCitizensImg,
-  stepsToApplyYourIndiaEvisaFromUsaImg,
+  applyIndiaTouristVisaForCanadianCitizensImg,
+  australiaVisaForUsCitizensImg,
   australiaVisaRequirementsForSingaporeCitizensImg,
   australianEtaForUkCitizensImg,
-  australiaVisaForUsCitizensImg,
-  oneYearMultiEntryNonImmigrantVisaUkToThailandImg,
+  bestThingsToDoInIndiaForAustraliaCitizensImg,
   completeThailandVisaGuideForUkCitizensImg,
+  essentialTipsForIndianBusinessVisa,
   howToApplyForThailandVisaFromUkImg,
-  travelToThailandFromUkImg,
-  sriLankanVisaRequirementsForCanadianCitizens,
+  howToGetTurkeyVisa,
+  howToGetYourIndiaTouristVisaForUsCitizensImg,
+  imageNotFound,
   indiaEVisaApplyOnline,
+  indianTouristVisaForUkCitizensImg,
+  indianVisaForEmiratisCitizensImg,
+  moroccoVisaEligibilityImg,
+  oneYearMultiEntryNonImmigrantVisaUkToThailandImg,
+  sriLankanVisaRequirementsForCanadianCitizens,
+  stepsToApplyYourIndiaEvisaFromUsaImg,
+  topThingsToDoInIndiaForFunAustraliaCitizensImg,
+  travelToThailandFromUkImg,
+  yourCompleteGuideImg,
 };
--- src/app/(visaCountries)/mainDirectoryHomePagesBlog/india/index.js
+++ src/app/(visaCountries)/mainDirectoryHomePagesBlog/india/index.js
@@ -1,11 +1,10 @@
 // india visa
-export { default as TopThingsToDoInIndiaForFunAustraliaCitizens } from './australia/top-things-to-do-in-india-for-fun.mdx';
 export { default as BestThingsToDoInIndiaForAustraliaCitizens } from './australia/best-things-to-do-in-india-for-australia-citizens.mdx';
+export { default as TopThingsToDoInIndiaForFunAustraliaCitizens } from './australia/top-things-to-do-in-india-for-fun.mdx';
 export { default as ApplyIndiaTouristVisaForCanadianCitizens } from './canada/apply-india-tourist-visa-for-canadian-citizens.mdx';
+export { default as EssentialTipsForIndianBusinessVisa } from './essential-tips-for-indian-business-visa.mdx';
 export { default as IndianVisaForEmiratisCitizens } from './uae/indian-visa-for-emiratis-citizens.mdx';
 export { default as ApplyIndiaEVisaFromUK } from './uk/apply-india-eVisa-from-uk.mdx';
 export { default as IndianTouristVisaForUKCitizens } from './uk/indian-tourist-visa-for-uk-citizens.mdx';
 export { default as HowToGetYourIndiaTouristVisaForUSCitizens } from './us/how-to-get-your-india-tourist-visa-for-us-citizens.mdx';
 export { default as StepsToApplyYourIndiaEVisaFromUSA } from './us/steps-to-apply-your-india-e-visa-from-usa.mdx';
-
-// australia visa
