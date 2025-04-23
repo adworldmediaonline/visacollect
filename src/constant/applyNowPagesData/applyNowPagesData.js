69770f974448afbc4044a3a2f0382d40690228a3
fastestsunil <sunil141292@gmail.com>
2025-04-24 00:37:08 +0530

    Remove jsconfig.json, update package dependencies, and refactor components for consistency

---
+++ src/constant/applyNowPagesData/applyNowPagesData.js
@@ -0,0 +1,66 @@
+export const touristVisaTypes = [
+  { label: '30 Days Single entry Visa', value: '30 Days Single entry Visa' },
+  {
+    label:
+      'Standard Visitor Visa (6 months) each stay restricted up to 60 days',
+    value:
+      'Standard Visitor Visa (6 months) each stay restricted up to 60 days',
+  },
+  {
+    label:
+      '1 Year Multiple Entry Tourist Visa each stay restricted upto 90 days',
+    value:
+      '1 Year Multiple Entry Tourist Visa each stay restricted upto 90 days',
+  },
+  {
+    label:
+      '2 Years Multiple Entry Visitor Visa - Tourist each stay restricted upto 180 days',
+    value:
+      '2 Years Multiple Entry Visitor Visa - Tourist each stay restricted upto 180 days',
+  },
+  {
+    label:
+      '5 Years Multiple entry visitor visa - Tourist each stay restricted upto 180 days',
+    value:
+      '5 Years Multiple entry visitor visa - Tourist each stay restricted upto 180 days',
+  },
+  {
+    label:
+      '10 Years Mutiple Entry Visitor Visa-Tourist each stay restricted upto 180 days',
+    value:
+      '10 Years Mutiple Entry Visitor Visa-Tourist each stay restricted upto 180 days',
+  },
+];
+
+export const businessVisaTypes = [
+  {
+    label:
+      'Standard Visitor Visa - Business (6 months) each stay restricted up to 60 days',
+    value:
+      'Standard Visitor Visa - Business (6 months) each stay restricted up to 60 days',
+  },
+  {
+    label:
+      '1 Year Multiple Entry Business Visa each stay restricted upto 90 days',
+    value:
+      '1 Year Multiple Entry Business Visa each stay restricted upto 90 days',
+  },
+  {
+    label:
+      '2 Years Multiple Entry Visitor Visa - Business each stay restricted upto 180 days',
+    value:
+      '2 Years Multiple Entry Visitor Visa - Business each stay restricted upto 180 days',
+  },
+  {
+    label:
+      '5 Years Multiple entry visitor visa - Business each stay restricted upto 180 days',
+    value:
+      '5 Years Multiple entry visitor visa - Business each stay restricted upto 180 days',
+  },
+  {
+    label:
+      '10 Years Mutiple Entry Visitor Visa - Business each stay restricted upto 180 days',
+    value:
+      '10 Years Mutiple Entry Visitor Visa - Business each stay restricted upto 180 days',
+  },
+];
